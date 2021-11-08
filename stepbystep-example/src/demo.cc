300028648ebf65a14679d17e1f3bb5f0e3cadf34
Piotr Sarna <sarna@scylladb.com>
2021-11-08 16:07:27 +0100

    lib: parametrize runners
    It's now possible to create multiple runners by initializing
    them with a numeric id.

--- stepbystep-example/src/demo.cc
+++ stepbystep-example/src/demo.cc
@@ -3,12 +3,17 @@
 #include <chrono>
 
 extern "C" {
-bool test_me_poll();
+bool test_me_poll(size_t idx);
+bool test_me_init(size_t idx);
+void put_payload(size_t idx, const char* payload);
 }
 
 int main() {
-    while (!test_me_poll()) {
+    test_me_init(0);
+    put_payload(3, "hello");
+    put_payload(7, "world");
+    while (!test_me_poll(0)) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        std::cout << "poll..." << std::endl;
+        std::cout << "<preempt happened>" << std::endl;
     }
 }
--- stepbystep-example/src/lib.rs
+++ stepbystep-example/src/lib.rs
@@ -1,11 +1,56 @@
-use async_std;
-use std::time::Duration;
-use stepbystep_export::export_async;
+use stepbystep::export_async;
+use std::ffi::CStr;
+use std::os::raw::c_char;
+use std::task::Poll;
+use std::task::Context;
+use std::future::Future;
+use std::pin::Pin;
+
+thread_local! {
+    static PAYLOAD_MAP: std::cell::RefCell<std::collections::HashMap<usize, String>> = std::cell::RefCell::new(std::collections::HashMap::new());
+}
+
+#[no_mangle]
+pub fn put_payload(idx: usize, ptr: *const c_char) {
+    let cstr = unsafe { CStr::from_ptr(ptr) };
+    let s = cstr.to_str().unwrap().to_owned();
+    PAYLOAD_MAP.with(|payload_map| {
+        payload_map.borrow_mut().insert(idx, s);
+    })
+}
+
+struct Yield {
+    ready: bool,
+}
+
+impl Future for Yield {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.ready {
+            Poll::Ready(())
+        } else {
+            self.ready = true;
+            Poll::Pending
+        }
+    }
+}
+
+pub async fn compute_complicated_stuff(i: usize) {
+    println!("Computation performed: {}*{}={}", i, i, i*i);
+    Yield{ready: false}.await
+}
 
 #[export_async]
-pub async fn test_me() {
+pub async fn test_me(instance_idx: usize) {
     for i in 1..10 {
-        println!("i={}", i);
-        async_std::task::sleep(Duration::from_secs(1)).await;
+        println!("Instance {}", instance_idx);
+        PAYLOAD_MAP.with(|payload_map| {
+            println!("i = {}; value = {}", i, match payload_map.borrow().get(&i) {
+                Some(s) => s,
+                None => "<none>",
+            });
+        });
+        compute_complicated_stuff(i).await;
     }
 }
