d5d57f54a5cdc679b13ee9f7f843185623337870
Piotr Sarna <sarna@scylladb.com>
2021-11-08 16:07:27 +0100

    lib: parametrize runners
    It's now possible to create multiple runners by initializing
    them with a numeric id.

---
+++ compile-demo-debug.sh
@@ -0,0 +1,1 @@
+g++ -o demo stepbystep-example/src/demo.cc target/debug/libstepbystep_example.a -lpthread -ldl
--- stepbystep/src/lib.rs
+++ stepbystep/src/lib.rs
@@ -6,21 +6,29 @@ pub fn export_async(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input: syn::ItemFn = syn::parse_macro_input!(item);
     let ident = &input.sig.ident;
     let poll_ident = format_ident!("{}_poll", ident);
+    let init_ident = format_ident!("{}_init", ident);
     let tokens = quote!{
         #input
 
         type Task = std::pin::Pin<Box<dyn std::future::Future<Output = ()>>>;
 
         thread_local! {
-            static RUNNER: std::cell::RefCell<Task> = std::cell::RefCell::new(std::boxed::Box::pin(#ident()));
+            static RUNNER: std::cell::RefCell<std::collections::HashMap<usize, Task>> = std::cell::RefCell::new(std::collections::HashMap::new());
         }
 
         #[no_mangle]
-        pub fn #poll_ident() -> bool {
+        pub fn #init_ident(idx: usize) {
+            RUNNER.with(|runner| {
+                runner.borrow_mut().insert(idx, std::boxed::Box::pin(#ident(idx)));
+            })
+        }
+
+        #[no_mangle]
+        pub fn #poll_ident(idx: usize) -> bool {
             RUNNER.with(|runner| {
                 let waker = futures::task::noop_waker();
                 let mut cx = std::task::Context::from_waker(&waker);
-                runner.borrow_mut().as_mut().poll(&mut cx).is_ready()
+                runner.borrow_mut().get_mut(&idx).unwrap().as_mut().poll(&mut cx).is_ready()
             })
         }
         
